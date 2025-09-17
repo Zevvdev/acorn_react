bc65bc29ecd91fc800d88ca651792fa0c28e119f
kimjieun <archi.01239@gmail.com>
2025-09-17 14:45:54 +0900

    main.jsx 에 redux 관련 설정

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,53 @@ import './index.css'
 //SPA 를 구현하기 위한 RouterProvider를 import
 import { RouterProvider} from 'react-router-dom'
 import router from './router'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux';
+
 
 
 //routing 정보를 담고있는 router import
 
+// redux store 에서 관리될 state 의 초기값
+const initState={
+  userInfo:null,
+  loginModal:{
+    title:null,
+    show:false
+  }
+};
 
+// reducer 함수에서 사용할 handler object
+const handlers = {
+  UPDATE_FORTUNE:(state, action)=>({
+    ...state,
+    fortuneToday:action.payload
+  }),
+  LOGIN_MODAL:(state, action)=>({
+    ...state,
+    loginModal:action.payload
+  }),
+  USER_INFO:(state, action)=>({
+    ...state,
+    userInfo:action.payload
+  })
+};
+
+// handler object 를 사용하는 새로운 reducer 함수
+const reducer = (state=initState, action)=>{
+  // 전달된 action을 수행할 handler 함수를 얻어낸다.
+  const handler = handlers[action.type];
+  // handler 함수가 존재한다면 handler 함수가 리턴하는 state 를 리턴하고
+  // 아니면 원래 state를 리턴한다.
+  return handler ? handler(state, action) : state;
+}
+
+// 위에서 만든 reducer 함수를 사용하는 redux store(중앙 저장소) 만들기
+const store = createStore(reducer);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}/>
     <RouterProvider router={router}/>
   </StrictMode>
 )
